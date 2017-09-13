9e23853872b9777902e1de0f66c39f490aebd822
DanielWLam <lwd931227@126.com>
2017-09-13 23:36:46 +0800

    加了个vue-markdown，但好像不怎么好用

---
+++ src/components/MarkdownOutputer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="markdown-body">
+    {{content}}
+  </div>
+</template>
+<script>
+  const fs = require('fs')
+  export default {
+    props: {
+      fileName: {
+        type: 'string'
+      }
+    },
+    computed: {
+      content: function () {
+        // generate-md --layout github --input ./input --output ./output
+        // src/markdown_src/output/filename
+        let file = fs.readFileSync(`../markdown_src/output/${this.fileName}`)
+        console.log(file)
+        return ''
+      }
+    }
+  }
+</script>
--- src/view/Color.vue
+++ src/view/Color.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="color">
-    color
+    <vue-markdown>
+      ### Solar System Exploration, 1950s – 1960s
+
+      - [ ] Mercury
+      - [x] Venus
+      - [x] Earth (Orbit/Moon)
+      - [x] Mars
+      - [ ] Jupiter
+      - [ ] Saturn
+      - [ ] Uranus
+      - [ ] Neptune
+      - [ ] Comet Haley
+    </vue-markdown>
   </div>
 </template>
+<script>
+  import VueMarkdown from 'vue-markdown'
+  export default {
+    components: {
+      VueMarkdown
+    }
+  }
+</script>

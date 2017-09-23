c0f8bc549f93817530f8111a7b2331fed474129e
DanielWLam <lwd931227@126.com>
2017-09-23 17:21:06 +0800

    reaction

--- src/view/Align.vue
+++ src/view/Align.vue
@@ -9,12 +9,105 @@
     </blockquote>
     <h2 id="文案类对齐">
       <a class="header-link" href="#文案类对齐"></a>文案类对齐</h2>
-    <p>当某个模块中的文字比较松散时，需要定一个统一的视觉起点，一般是左对齐</p>
+      <Row>
+        <Col span="12">
+          <p>当某个模块中的文字比较松散时，需要定一个统一的视觉起点，一般是左对齐</p>
+        </Col>
+        <Col span="12" class="display">
+          <Row :gutter="16">
+            <Col span="12">
+              <div class="correct">
+                <card>
+                  <img src="https://os.alipayobjects.com/rmsportal/xvmiAZAIxrEcqdP.png" alt="">
+                </card>
+                <p>推荐（图片来自Ant Design）</p>
+              </div>
+            </Col>
+            <Col span="12">
+              <div class="wrong">
+                <card>
+                  <img src="https://os.alipayobjects.com/rmsportal/qvycImsTiDGVgLJ.png" alt="">
+                </card>
+                <p>不推荐（图片来自Ant Design）</p>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     <h2 id="表单类对齐">
       <a class="header-link" href="#表单类对齐"></a>表单类对齐</h2>
-    <p>表单设计中经常会出现如右图所示的表单项，通过冒号对齐（右对齐）能使内容锁定在一定的范围内，保证右边的输入项整齐，是界面看上去更美观，对于用户来说，也能提升阅读速度和录入速度。</p>
+      <Row :gutter="16">
+        <Col span="12">
+          <p>表单设计中经常会出现如右图所示的表单项，通过冒号对齐（右对齐）能使内容锁定在一定的范围内，保证右边的输入项整齐，是界面看上去更美观，对于用户来说，也能提升阅读速度和录入速度。</p>
+        </Col>
+        <Col span="12"  class="display">
+          <Row>
+            <Col span="24">
+              <card>
+                <img src="https://os.alipayobjects.com/rmsportal/DmEbaUsrpJkRyUh.png" alt="">
+              </card>
+              <p>图片来自（Ant Design）</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     <h2 id="数字类对齐">
       <a class="header-link" href="#数字类对齐"></a>数字类对齐</h2>
-    <p>通常数字需要比较，建议全局设置数字的有效位数为2位小数，并右对齐</p>
+      <Row>
+        <Col span="12">
+          <p>通常数字需要比较，建议全局设置数字的有效位数为2位小数，并右对齐</p>
+        </Col>
+        <Col span="12" class="display">
+          <Row :gutter="16">
+            <Col span="12">
+              <div class="correct">
+                <card>
+                  <img src="https://os.alipayobjects.com/rmsportal/hCSQTEmahFyQcWk.png" alt="">
+                </card>
+                <p>推荐（图片来自Ant Design）</p>
+              </div>
+            </Col>
+            <Col span="12">
+              <div class="wrong">
+                <card>
+                  <img src="https://os.alipayobjects.com/rmsportal/FDjScmPlWFPxkxL.png" alt="">
+                </card>
+                <p>不推荐（图片来自Ant Design）</p>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
   </article>
 </template>
+<script>
+  import Card from '../components/Card.vue'
+  export default {
+    components: {
+      Card
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .display.ivu-col {
+    p {
+      text-align: center;
+      font-size: 12px;
+      margin-top: 8px;
+    }
+  }
+  .correct {
+    p {
+      border-top: 5px solid #108ee9;
+      text-align: center;
+      font-size: 12px;
+    }
+  }
+  .wrong {
+    p {
+      border-top: 5px solid #f04134;
+      text-align: center;
+      font-size: 12px;
+    }
+  }
+</style>
--- src/view/Reaction.vue
+++ src/view/Reaction.vue
@@ -1,16 +1,90 @@
 <template>
   <article class="markdown-body">
-    <h1 id="即时反应"><a class="header-link" href="#即时反应"></a>即时反应</h1>
+    <h1 id="即时反应">
+      <a class="header-link" href="#即时反应"></a>即时反应</h1>
     <p>用户在对系统做出操作后，要及时地给出反应。不能给出反应或者说不能交互的事物，不应该引起用户的注意，甚至点击。 反馈太少甚至没有反馈的系统，会让人觉得系统迟钝，生硬。
     </p>
-    <h2 id="查询式反应"><a class="header-link" href="#查询式反应"></a>查询式反应</h2>
-    <p>在用户进行搜索等操作时，给出可能的匹配项</p>
-    <h2 id="反馈型反应"><a class="header-link" href="#反馈型反应"></a>反馈型反应</h2>
-    <ol class="list">
-      <li>实时预览 在表单项中，要进行必要的数据校验，并及时给出反馈。解决错误的最好方法就是不让错误发生。
-      </li>
-      <li>进度提示 在前后端通信过程中，由于网络的原因，有些操作需要一定的事件完成，这时要告知用户进度，让用户保持“知情权”，让他们安心。
-      </li>
-    </ol>
+    <h2 id="查询式反应">
+      <a class="header-link" href="#查询式反应"></a>查询式反应</h2>
+    <Row :gutter="16">
+      <Col span="12">
+      <p>在用户进行搜索等操作时，给出可能的匹配项</p>
+      </Col>
+      <Col span="12" class="display">
+      <Row>
+        <Col span="24">
+        <card>
+          <img src="https://os.alipayobjects.com/rmsportal/fgQfkNakHrUiAun.png" alt="">
+        </card>
+        <p>图片来自（Ant Design）</p>
+        </Col>
+      </Row>
+      </Col>
+    </Row>
+    <h2 id="反馈型反应">
+      <a class="header-link" href="#反馈型反应"></a>反馈型反应</h2>
+    <div class="list">
+      <Row :gutter="16">
+        <Col span="12">1. 实时预览 在表单项中，要进行必要的数据校验，并及时给出反馈。解决错误的最好方法就是不让错误发生。
+        </Col>
+        <Col span="12" class="display">
+        <Row>
+          <Col span="24">
+          <card>
+            <img src="https://os.alipayobjects.com/rmsportal/jecYhRgfbHleGDJ.png" alt="">
+          </card>
+          <p>图片来自（Ant Design）</p>
+          </Col>
+        </Row>
+        </Col>
+      </Row>
+      <Row :gutter="16">
+        <Col span="12">2. 进度提示 在前后端通信过程中，由于网络的原因，有些操作需要一定的事件完成，这时要告知用户进度，让用户保持“知情权”，让他们安心。
+        </Col>
+        <Col span="12" class="display">
+        <Row>
+          <Col span="24">
+          <card>
+            <img src="https://os.alipayobjects.com/rmsportal/FBAZGqfeUnDlUtw.png" alt="">
+          </card>
+          <p>图片来自（Ant Design）</p>
+          </Col>
+        </Row>
+        </Col>
+      </Row>
+    </div>
   </article>
 </template>
+<script>
+import Card from '../components/Card.vue'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+<style lang="less" scoped>
+.display.ivu-col {
+  p {
+    text-align: center;
+    font-size: 12px;
+    margin-top: 8px;
+  }
+}
+
+.correct {
+  p {
+    border-top: 5px solid #108ee9;
+    text-align: center;
+    font-size: 12px;
+  }
+}
+
+.wrong {
+  p {
+    border-top: 5px solid #f04134;
+    text-align: center;
+    font-size: 12px;
+  }
+}
+</style>
